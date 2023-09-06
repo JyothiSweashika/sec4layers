bc4089128b157152b3f9318a88419a84a255503d
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-09-06 07:53:38 +0530

    assisted

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _1._33_EF_using_Data_Access_Layers
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1._33_EF_using_Data_Access_Layers.MvcApplication" Language="C#" %>
